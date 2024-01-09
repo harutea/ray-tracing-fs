52338d5afb5edae48f6dc28312c448451caea735
harutea <snowhanami@gmail.com>
2024-01-09 13:43:08 +0900

    implement diffuse material

--- app/shaders/ray-tracing-fragment.glsl
+++ app/shaders/ray-tracing-fragment.glsl
@@ -6,10 +6,10 @@ const float pi = 3.14159265;
 
 // utility
 
-float variation = 0.001;
+float variation = 0.0001;
 float rand(vec2 co){
-    variation += 0.001;
-    return fract(sin(dot(co, vec2(12.9898+variation, 78.233+variation))) * 43758.5453);
+    variation += 0.0001;
+    return fract(sin(dot(co, vec2(12.9898+variation, 78.2330+variation))) * 43758.5453);
 }
 
 float rand(vec2 co, float min, float max) {
@@ -23,6 +23,27 @@ vec3 rand_vec3(vec2 co) {
 vec3 rand_vec3(vec2 co, float min, float max) {
   return vec3(rand(co, min, max), rand(co, min, max), rand(co, min, max));
 }
+
+vec3 random_in_unit_sphere(vec2 co) {
+  while (true) {
+    vec3 p = rand_vec3(co, -1.0, 1.0);
+    if(length(p) < 1.0)
+      return p;
+  }
+}
+
+vec3 random_unit_vector(vec2 co) {
+  return normalize(random_in_unit_sphere(co));
+}
+
+vec3 random_on_hemisphere(vec2 co, vec3 normal) {
+  vec3 on_unit_sphere = random_unit_vector(co);
+  if (dot(on_unit_sphere, normal) > 0.0)
+    return on_unit_sphere;
+  else
+    return -on_unit_sphere;
+}
+
 
 float degrees_to_radians(float degrees) {
   return degrees * pi / 180.0;
@@ -101,36 +122,45 @@ struct Triangle {
 };
 
 vec3 ray_color(Ray ray) {
-  
+  int depth = 50;
+
   // world
   Sphere[] sphere_list = Sphere[](
-    Sphere(vec3(0.0, 0.0, -1.0), 0.5),
+    Sphere(vec3(0.0, 0.1 * sin(u_time*2.0), -1.0), 0.5),
     Sphere(vec3(0.0, -100.5, -1.0), 100.0)
   );
 
-  IntersectionRecord record;
-  IntersectionRecord rec_temp;
-  bool intersect_something = false;
-  float closest = infinity;
-  
-  for (int i=0; i<sphere_list.length(); i++) {
-    bool intersect = ray_sphere_intersection(ray, Interval(0.0, closest), sphere_list[i], rec_temp);
-    
-    if(intersect) {
-      intersect_something = true;
-      closest = rec_temp.t;
-      record = rec_temp;
-    }
-  }
-
-  if(intersect_something) {
-      return 0.5 * (record.normal + vec3(1.0, 1.0, 1.0));
-  }
-
-  // intersect with nothing
   vec3 unit_direction = normalize(ray.direction);
   float a = 0.5*(unit_direction.y + 1.0);
-  return (1.0-a)*vec3(1.0, 1.0, 1.0) + a*vec3(0.5, 0.7, 1.0);
+  vec3 final_color = (1.0-a)*vec3(1.0, 1.0, 1.0) + a*vec3(0.5, 0.7, 1.0);
+
+  Ray ray_next = ray;
+  int ray_i = 0;
+  for(ray_i = 0; ray_i < depth; ray_i++) {
+    IntersectionRecord record;
+    IntersectionRecord rec_temp;
+    bool intersect_something = false;
+    float closest = infinity;
+    
+    for (int i=0; i<sphere_list.length(); i++) {
+      bool intersect = ray_sphere_intersection(ray_next, Interval(0.001, closest), sphere_list[i], rec_temp);
+      
+      if(intersect) {
+        intersect_something = true;
+        closest = rec_temp.t;
+        record = rec_temp;
+      }
+    }
+
+    if(!intersect_something)
+      break;
+    
+    vec3 direction = record.normal + random_unit_vector(vec2(gl_FragCoord.xy));
+    ray_next = Ray(record.pos, direction);
+    final_color = 0.5 * final_color;
+  }
+
+  return final_color;
 }
 
 struct Camera {
