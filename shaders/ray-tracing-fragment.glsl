a463e8724af976ecf1006772a668d1392736ad7c
harutea <snowhanami@gmail.com>
2024-01-09 23:34:05 +0900

    define Material, implement metal

--- shaders/ray-tracing-fragment.glsl
+++ shaders/ray-tracing-fragment.glsl
@@ -1,10 +1,12 @@
 uniform vec2 u_resolution;
 uniform float u_time;
 
-const float infinity = 10000000.0;
-const float pi = 3.14159265;
+const float INFINITY = 10000000.0;
+const float PI = 3.14159265;
+const int MAX_DEPTH = 50;
+const int SAMPLES_PER_PIXEL = 100;
 
-// utility
+// Utility
 
 float variation = 0.00001;
 float rand(vec2 co){
@@ -44,8 +46,13 @@ vec3 random_on_hemisphere(vec2 co, vec3 normal) {
     return -on_unit_sphere;
 }
 
+bool near_zero(vec3 v) {
+  float s = 1e-8;
+  return (abs(v.x) < s) && (abs(v.y) < s) && (abs(v.z) < s);
+}
+
 float degrees_to_radians(float degrees) {
-  return degrees * pi / 180.0;
+  return degrees * PI / 180.0;
 }
 
 float linear_to_gamma(float linear_component) {
@@ -70,8 +77,11 @@ float interval_clamp(Interval interval, float x) {
   return x;
 }
 
-Interval empty = Interval(+infinity, -infinity);
-Interval universe = Interval(-infinity, +infinity);
+Interval empty = Interval(+INFINITY, -INFINITY);
+Interval universe = Interval(-INFINITY, +INFINITY);
+
+
+// Ray
 
 struct Ray {
   vec3 origin;
@@ -82,15 +92,39 @@ vec3 ray_at(Ray ray, float t) {
   return ray.origin + t*ray.direction;
 }
 
+
+// Material
+
+struct Material {
+  int type;
+  vec3 albedo;
+};
+
+
+// Intersection Record
+
 struct IntersectionRecord {
   vec3 pos;
   vec3 normal;
   float t;
+  bool front_face;
+  Material material;
 };
+
+void set_intersected_face_normal(inout IntersectionRecord record, Ray ray, vec3 outward_normal) {
+  // outward_normal : unit vector
+
+  record.front_face = dot(ray.direction, outward_normal) < 0.0;
+  record.normal = record.front_face ? outward_normal : -outward_normal;
+}
+
+
+// Geometry
 
 struct Sphere {
   vec3 center;
   float radius;
+  Material material;
 };
 
 bool ray_sphere_intersection(Ray ray, Interval ray_t, Sphere s, inout IntersectionRecord record) {
@@ -114,7 +148,9 @@ bool ray_sphere_intersection(Ray ray, Interval ray_t, Sphere s, inout Intersecti
 
   record.t = root;
   record.pos = ray_at(ray, record.t);
-  record.normal = normalize(record.pos - s.center);
+  vec3 outward_normal = (record.pos - s.center) / s.radius;
+  set_intersected_face_normal(record, ray, outward_normal);
+  record.material = s.material;
   return true;
 }
 
@@ -124,47 +160,96 @@ struct Triangle {
   vec3 p3;
 };
 
+
+// Scatter Ray
+
+bool scatter(Material material, Ray ray_in, IntersectionRecord record, inout vec3 attenuation, inout Ray scattered) {
+  
+  if (material.type == 0) {
+
+    // Lambertian
+    vec3 scatter_direction = record.normal + random_unit_vector(vec2(gl_FragCoord.xy));
+    if(near_zero(scatter_direction))
+      scatter_direction = record.normal;
+    scattered = Ray(record.pos, scatter_direction);
+    attenuation = material.albedo;
+    return true;
+
+  } else if (material.type == 1) {
+
+    // Metal
+    vec3 reflected = reflect(normalize(ray_in.direction), record.normal);
+    scattered = Ray(record.pos, reflected);
+    attenuation = material.albedo;
+    return true;
+
+  }
+  
+  return false;
+
+}
+
+
+// Pixel Color
+
 vec3 ray_color(Ray ray) {
-  int depth = 50;
+  int max_depth = MAX_DEPTH;
 
   // world
   Sphere[] sphere_list = Sphere[](
-    Sphere(vec3(0.0, 0.1 * sin(u_time*2.0), -1.0), 0.5),
-    Sphere(vec3(0.0, -100.5, -1.0), 100.0)
+    Sphere(vec3(0.0, 0.0, -1.0), 0.5, Material(0, vec3(0.7, 0.3, 0.3))),
+    Sphere(vec3(0.0, -100.5, -1.0), 100.0, Material(0, vec3(0.8, 0.8, 0.0))),
+    Sphere(vec3(-1.0, 0.0, -1.0), 0.5, Material(1, vec3(0.8, 0.8, 0.8))),
+    Sphere(vec3(1.0, 0.0, -1.0), 0.5, Material(1, vec3(0.8, 0.6, 0.2)))
   );
 
   vec3 unit_direction = normalize(ray.direction);
   float a = 0.5*(unit_direction.y + 1.0);
   vec3 final_color = (1.0-a)*vec3(1.0, 1.0, 1.0) + a*vec3(0.5, 0.7, 1.0);
 
-  Ray ray_next = ray;
-  int ray_i = 0;
-  for(ray_i = 0; ray_i < depth; ray_i++) {
+  Ray ray_curr = ray;
+  int curr_depth = 0;
+  for(curr_depth = 0; curr_depth < max_depth; curr_depth++) {
     IntersectionRecord record;
     IntersectionRecord rec_temp;
     bool intersect_something = false;
-    float closest = infinity;
+    float closest = INFINITY;
     
+    // Test Ray-Sphere Intersection
     for (int i=0; i<sphere_list.length(); i++) {
-      bool intersect = ray_sphere_intersection(ray_next, Interval(0.001, closest), sphere_list[i], rec_temp);
-      
-      if(intersect) {
+      if(ray_sphere_intersection(ray_curr, Interval(0.001, closest), sphere_list[i], rec_temp)) {
         intersect_something = true;
         closest = rec_temp.t;
         record = rec_temp;
       }
     }
 
+    // Test Ray-Triangle Intersection
+    
+    
     if(!intersect_something)
       break;
     
-    vec3 direction = record.normal + random_unit_vector(vec2(gl_FragCoord.xy));
-    ray_next = Ray(record.pos, direction);
-    final_color = 0.5 * final_color;
+        
+    Ray scattered;
+    vec3 attenuation;
+    bool is_scattered = scatter(record.material, ray_curr, record, attenuation, scattered);
+
+    if(!is_scattered)
+      return vec3(0.0, 0.0, 0.0);
+
+    ray_curr = scattered;
+    final_color = attenuation * final_color;
   }
+
+  if(curr_depth == max_depth)
+    return vec3(0.0, 0.0, 0.0);
 
   return final_color;
 }
+
+
+// Camera
 
 struct Camera {
   float aspect_ratio;
@@ -177,7 +262,7 @@ struct Camera {
 
 void init(inout Camera camera) {
   camera.aspect_ratio = u_resolution.y/u_resolution.x;
-  camera.samples_per_pixel = 100;
+  camera.samples_per_pixel = SAMPLES_PER_PIXEL;
 
   float focal_length = 1.0;
   float viewport_height = 2.0;
@@ -207,6 +292,7 @@ Ray get_ray(Camera camera, float x, float y) {
   vec3 ray_direction = pixel_sample - camera.center;
   return Ray(camera.center, ray_direction);
 }
+
 
 void main() {
   Camera camera;
